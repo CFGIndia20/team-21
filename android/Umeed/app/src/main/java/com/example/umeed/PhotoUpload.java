2609cbf0d1b15591b3c3b53cb138572fc2bb5ef2
MohitThorat <2017.mohit.thorat@ves.ac.in>
2020-07-26 05:13:51 +0530

    Image shown succesfully

--- android/Umeed/app/src/main/java/com/example/umeed/PhotoUpload.java
+++ android/Umeed/app/src/main/java/com/example/umeed/PhotoUpload.java
@@ -111,7 +111,6 @@ public class PhotoUpload extends AppCompatActivity {
                                 cursor.close();
                             }
                         }
-
                     }
                     break;
             }
